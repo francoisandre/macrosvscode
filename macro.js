7dbc4e9c797437227f3d3c4bb12113f8e8e448ce
Francois ANDRE <francois.andre@obs-mip.fr>
2022-12-10 15:50:19 +0100

    Fix focusLine

--- macro.js
+++ macro.js
@@ -1,99 +1,218 @@
-const vscode = require('vscode');
+const vscode = require("vscode");
 
+let steps = [
+  "<template>",
+  "</template>",
+  "<script>",
+  "name:",
+  "computed:",
+  "props:",
+  "watch:",
+  "data()",
+  "mounted()",
+  "methods:",
+  "</script>",
+  "<style>",
+  "</style",
+];
 
 module.exports.macroCommands = {
-   GoToData: {
-      no: 1,
-      func: gotoData
-   },
-   GoToComputed: {
-      no: 2,
-      func: gotoComputed
-   },
-   GoToMounted: {
-      no: 3,
-      func: gotoMounted
-   },
-   CreateData: {
-      no: 4,
-      func: createData
-   }
-}
+  ScrollDown: {
+    no: 1,
+    func: scrollDown,
+  },
+  ScrollUp: {
+    no: 2,
+    func: scrollUp,
+  },
+  CreateData: {
+    no: 3,
+    func: createData,
+  },
+  CreateMethod: {
+    no: 4,
+    func: createMethod,
+  },
+};
 
 function getLine(pattern) {
-   let editor = vscode.window.activeTextEditor;
-   let doc = editor.document; 
-   let lineCount = doc.lineCount;
-   let found = false;
-   for (let i = 0; i< lineCount; i++) {
-      lineIndex = i;
-      let line = doc.lineAt(i);
-      if (! line.isEmptyOrWhitespace) {
-         let text = line.text;
-         if (text.indexOf(pattern)>=0) {
-            return i;
-         }
+  let editor = vscode.window.activeTextEditor;
+  let doc = editor.document;
+  let lineCount = doc.lineCount;
+  let found = false;
+  for (let i = 0; i < lineCount; i++) {
+    lineIndex = i;
+    let line = doc.lineAt(i);
+    if (!line.isEmptyOrWhitespace) {
+      let text = line.text;
+      if (text.indexOf(pattern) >= 0) {
+        return i;
       }
-   }
+    }
+  }
 
-   return -1;
+  return -1;
 }
 
 function goTo(pattern) {
-   let editor = vscode.window.activeTextEditor;
-   let doc = editor.document; 
-   let lineCount = doc.lineCount;
-   let lineIndex = 0;
-   let found = false;
-   for (let i = 0; i< lineCount; i++) {
-      lineIndex = i;
-      let line = doc.lineAt(i);
-      if (! line.isEmptyOrWhitespace) {
-         let text = line.text;
-         if (text.indexOf(pattern)>=0) {
-            found = true;
-            break
-         }
+  let editor = vscode.window.activeTextEditor;
+  let doc = editor.document;
+  let lineCount = doc.lineCount;
+  let lineIndex = 0;
+  let found = false;
+  for (let i = 0; i < lineCount; i++) {
+    lineIndex = i;
+    let line = doc.lineAt(i);
+    if (!line.isEmptyOrWhitespace) {
+      let text = line.text;
+      if (text.indexOf(pattern) >= 0) {
+        found = true;
+        break;
       }
-   }
+    }
+  }
 
-   if (found) {
-   let line = doc.lineAt(lineIndex);
-   let range = line.range;
-   editor.selection =  new vscode.Selection(range.start, range.end);
-   editor.revealRange(range, vscode.TextEditorRevealType.AtTop);
-   }
+  if (found) {
+    let line = doc.lineAt(lineIndex);
+    let range = line.range;
+    editor.selection = new vscode.Selection(range.start, range.end);
+    editor.revealRange(range, vscode.TextEditorRevealType.AtTop);
+  }
+}
+
+function focusLine(lineIndex) {
+  if (lineIndex < 0) {
+    return;
+  }
+  let editor = vscode.window.activeTextEditor;
+  let doc = editor.document;
+  let line = doc.lineAt(lineIndex);
+  let range = line.range;
+  editor.selection = new vscode.Selection(range.start, range.end);
+  editor.revealRange(range, vscode.TextEditorRevealType.AtTop);
+}
+
+function findLine(pattern) {
+  let editor = vscode.window.activeTextEditor;
+  let doc = editor.document;
+  let lineCount = doc.lineCount;
+  let lineIndex = 0;
+  let found = false;
+  for (let i = 0; i < lineCount; i++) {
+    lineIndex = i;
+    let line = doc.lineAt(i);
+    if (!line.isEmptyOrWhitespace) {
+      let text = line.text;
+      if (text.indexOf(pattern) >= 0) {
+        return i;
+      }
+    }
+  }
+  return -1;
+}
+
+function getCurrentLine() {
+  const activeEditor = vscode.window.activeTextEditor;
+  if (activeEditor) {
+    return activeEditor.selection.active.line;
+  } else {
+    return -1;
+  }
+}
+
+function scrollDown() {
+  let current = getCurrentLine();
+  for (let i = 0; i < steps.length; i++) {
+    let next = findLine(steps[i]);
+    if (next > current) {
+      focusLine(next);
+      return;
+    }
+  }
+  focusLine(0);
+}
+
+function scrollUp() {
+  let current = getCurrentLine();
+  for (let i = steps.length - 1; i >= 0; i--) {
+    let previous = findLine(steps[i]);
+    if (previous < current) {
+      focusLine(previous);
+      return;
+    }
+  }
+  let editor = vscode.window.activeTextEditor;
+  let doc = editor.document;
+  let lineCount = doc.lineCount;
+  focusLine(lineCount - 1);
 }
 
 function createData() {
-   let editor = vscode.window.activeTextEditor;
-   const selection = editor.selection;
-   if (selection && !selection.isEmpty) {
-      const selectionRange = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
-      const highlighted = editor.document.getText(selectionRange);
-      let lineNumber = getLine("data()")
-      if (lineNumber == -1) {
-         return;
-      } else {
-         const editRange = editor.document.lineAt(lineNumber+1).range.end;
-         editor.edit(editBuilder =>{
-            if (editor !== undefined){
-              editBuilder.insert(editRange,"\n"+highlighted+": null,");
-            }
-        });
-      }
+  let editor = vscode.window.activeTextEditor;
 
-   }
+  const selection = editor.selection;
+  if (selection && !selection.isEmpty) {
+    const selectionRange = new vscode.Range(
+      selection.start.line,
+      selection.start.character,
+      selection.end.line,
+      selection.end.character
+    );
+    const highlighted = editor.document.getText(selectionRange);
+    let lineNumber = getLine("data()");
+    if (lineNumber == -1) {
+      return;
+    } else {
+      const editRange = editor.document.lineAt(lineNumber + 1).range.end;
+      editor.edit((editBuilder) => {
+        if (editor !== undefined) {
+          editBuilder.insert(editRange, "\n" + highlighted + ": null,");
+          vscode.commands.executeCommand("editor.action.formatDocument");
+        }
+      });
+    }
+  }
+}
+
+function createMethod() {
+  let editor = vscode.window.activeTextEditor;
+
+  const selection = editor.selection;
+  if (selection && !selection.isEmpty) {
+    const selectionRange = new vscode.Range(
+      selection.start.line,
+      selection.start.character,
+      selection.end.line,
+      selection.end.character
+    );
+    const highlighted = editor.document.getText(selectionRange);
+    let lineNumber = getLine("methods:");
+    if (lineNumber == -1) {
+      return;
+    } else {
+      const editRange = editor.document.lineAt(lineNumber).range.end;
+      editor.edit((editBuilder) => {
+        if (editor !== undefined) {
+          editBuilder.insert(editRange, "\n" + highlighted + "() {},");
+          vscode.commands.executeCommand("editor.action.formatDocument");
+        }
+      });
+    }
+  }
+}
+
+async function format() {
+  await vscode.commands.executeCommand("editor.action.formatDocument");
 }
 
 function gotoData() {
-   goTo("data()")
+  goTo("data()");
 }
 
 function gotoComputed() {
-   goTo("computed:")
+  goTo("computed:");
 }
 
 function gotoMounted() {
-   goTo("mounted()")
+  goTo("mounted()");
 }
